4a793cf6912b66f2af8656c245378ceb7e3b763b
River Hansen <riverbh01@gmail.com>
2023-05-15 14:25:10 -0600

    Use displayCountry state in the React Application

--- src/Components/OptionDisplay.js
+++ src/Components/OptionDisplay.js
@@ -1,14 +1,22 @@
 import React from "react";
-import { useSelector } from "react-redux";
+import { useSelector, useDispatch } from "react-redux";
 import { selectPotentials } from "../redux/slices/potentialCountriesSlice";
+import { setDisplayCountry } from "../redux/slices/displayCountrySlice";
 
 const OptionDisplay = () => {
     let currentPotentials = useSelector(selectPotentials)
     console.log(currentPotentials)
+    let dispatch = useDispatch()
 
     return <div className="stack">{currentPotentials.map((e, i) => {
         return (
-            <h2 key={e.name.official} className="country-option">
+            <h2 
+                onClick={() => {
+                    dispatch(setDisplayCountry(currentPotentials[i]))
+                }} 
+                key={e.name.official} 
+                className="country-option"
+            >
                 {e.name.common}
             </h2>
         )
